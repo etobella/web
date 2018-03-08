8f61b96fead1eca96ebac812bd8021393a236278
Enric Tobella <etobella@creublanca.es>
2018-03-08 12:04:02 +0100

    TEST Of JSColor fix

--- web_widget_color/static/src/js/widget.js
+++ web_widget_color/static/src/js/widget.js
@@ -5,15 +5,7 @@ odoo.define('web.web_widget_color', function(require) {
     var field_registry = require('web.field_registry');
     var FormController = require('web.FormController');
     var field_utils = require('web.field_utils');
-
-    var _super_getDir = jscolor.getDir.prototype;
-    jscolor.getDir = function () {
-        var dir = _super_getDir.constructor();
-        if (dir.indexOf('web_widget_color') === -1) {
-            jscolor.dir = '/web_widget_color/static/lib/jscolor/';
-        }
-        return jscolor.dir;
-    };
+    var ViewManager = require('web.ViewManager');
 
     var FieldColor = basic_fields.FieldChar.extend({
         template: 'FieldColor',
@@ -23,7 +15,6 @@ odoo.define('web.web_widget_color', function(require) {
         },
         _render: function () {
             var show_value = field_utils.format.char(this.value);
-            jscolor.init(this.$el[0]);
             if (this.mode !== 'readonly') {
                 var $input = this.$el.find('input');
                 $input.val(show_value);
@@ -44,10 +35,18 @@ odoo.define('web.web_widget_color', function(require) {
      * Init jscolor for each editable mode on view form
      */
     FormController.include({
-        _updateEnv : function (parentID) {
-            this._super(parentID);
-            jscolor.init(this.$el[0]);
+        _updateEnv : function () {
+            this._super();
+            jscolor.installByClassName("jscolor");
+        },
+        _confirmChange: function (id, fields, e) {
+            this._super(id, fields, e);
+            jscolor.installByClassName("jscolor");
         }
     });
     return FieldColor
+});
+
+$(document).ready(function() {
+    jscolor.installByClassName("jscolor");
 });
